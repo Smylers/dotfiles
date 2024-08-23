a998e4c8859e6d1ea8c964bc09e26e68b990612a
Smylers <Smylers@stripey.com>
2024-08-23 17:43:44 +0100

    Enhancement: No per-tab close buttons
    Get rid of close buttons on individual tabs, since they get in the way.
    Even just having one on the current tab reduces its useful area and leads
    to its sometimes getting clicked on by mistake.
    
    Middle-click already works anywhere to close a tab, and I have a close
    button at the right of the tab bar.
    
    This involves creating userChrome.css, linking it into the random profile
    directory, and the preference for telling Firefox to look for
    userChrome.css. Fortunately the preference can easily be added to user.js
    created for something else, and the chrome directory can be linked in like
    user.js is with a little modification to the script.

--- tag-desktop/mozilla/firefox/user.js
+++ tag-desktop/mozilla/firefox/user.js
@@ -3,5 +3,9 @@
 // This needs linking into ~/.mozilla/firefox/*.default*/ and then it will
 // override settings in prefs.js.
 
+
 // Don't automatically play videos:
 user_pref("media.autoplay.default", 5);
+
+// Heed userChrome.css and userContent.css:
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
