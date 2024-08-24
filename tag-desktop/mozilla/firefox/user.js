e3221f2e9096be21758fab30e056560430497fdc
Smylers <Smylers@stripey.com>
2024-08-24 07:28:27 +0100

    Enhancement: Firefox preferences
    Better behaviour for Ctrl+Tab, Tab, zooming, and animated images.

--- tag-desktop/mozilla/firefox/user.js
+++ tag-desktop/mozilla/firefox/user.js
@@ -9,3 +9,15 @@ user_pref("media.autoplay.default", 5);
 
 // Heed userChrome.css and userContent.css:
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Ctrl+Tab switches between most recently used tabs:
+user_pref("browser.ctrlTab.sortByRecentlyUsed", true);
+
+// Ctrl+Plus increases the text size only:
+user_pref("browser.zoom.full", true);
+
+// Tab goes straight to form fields, not through links:
+user_pref("accessibility.tabfocus", 3);
+
+// Stop animating images once they've been through once:
+user_pref("image.animation_mode", 'once');
